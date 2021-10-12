6f04ec727412cd7db691eaafd05f75a9bf83353d
Shivek Khurana <khuranashivek@outlook.com>
2021-10-12 08:35:17 +0530

    ✨ Init

--- source/Popup/Popup.jsx
+++ source/Popup/Popup.jsx
@@ -1,53 +1,21 @@
-import * as React from 'react';
-import browser from 'webextension-polyfill';
+import * as React from "react";
+import browser from "webextension-polyfill";
+import Iframe from "react-iframe";
 
-import './styles.scss';
+import "./styles.scss";
 
 function openWebPage(url) {
-  return browser.tabs.create({url});
+  return browser.tabs.create({ url });
 }
 
 const Popup = () => {
   return (
     <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
-      <button
-        id="options__button"
-        type="button"
-        onClick={() => {
-          return openWebPage('options.html');
-        }}
-      >
-        Options Page
-      </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={() => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={() => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
+      <Iframe
+        url="http://localhost:3000/browser-extension"
+        width="400"
+        height="400"
+      />
     </section>
   );
 };
